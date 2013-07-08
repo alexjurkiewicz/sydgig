e92ecf91762cd9c098171cf466067705b5908fc8
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-07-08 11:50:35 +1000

    Add lastfm API

---
+++ lastfm_api.py
@@ -0,0 +1,10 @@
+import lastfm
+
+API_KEY = "2ed2ceec8bcde152af82eb77efd50eee"
+API_SECRET = "7a10b0010b9a0490817b27ea3d315839"
+
+api = lastfm.Api(API_KEY)
+
+def get_artist_info(name):
+    '''Given an artist name, return last.fm info about them'''
+    return api.get_artist(name)
