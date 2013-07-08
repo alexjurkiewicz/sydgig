2b723c5bb47f98811f63dbdac3aa218862ad3e98
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-07-08 12:25:47 +1000

    controller.py is actually the model

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
 from template import templates
-import controller
+import model
 
 @app.route("/")
 def index():
@@ -17,20 +17,20 @@ def index():
 @app.route('/artist/')
 def artist():
     template = templates.get_template("artist.html")
-    return template.render(artists=controller.get_artists())
+    return template.render(artists=model.get_artists())
 
 @app.route('/artist/<int:id>/<name>')
 @app.route('/artist/<int:id>/')
 def artist_info(id, name=None):
     template = templates.get_template("artist_info.html")
-    return template.render(artist=controller.get_artist_by_id(id))
+    return template.render(artist=model.get_artist_by_id(id))
 
 @app.route('/artist/add/', methods=['GET', 'POST'])
 def artist_add():
     name = request.form.get('name')
     bio = request.form.get('bio')
     if name and bio:
-        controller.add_artist(name, bio)
+        model.add_artist(name, bio)
         return redirect(url_for('artist'))
     else:
         template = templates.get_template("artist_add.html")
@@ -40,7 +40,7 @@ def artist_add():
 def artist_delete():
     id = request.form.get('id')
     if id:
-        controller.delete_artist(id)
+        model.delete_artist(id)
         return redirect(url_for('artist'))
     else:
         template = templates.get_template("artist_delete.html")
@@ -50,19 +50,19 @@ def artist_delete():
 @app.route('/venue/')
 def venue():
     template = templates.get_template("venue.html")
-    return template.render(venues=controller.get_venues())
+    return template.render(venues=model.get_venues())
 
 @app.route('/venue/<int:id>/<name>')
 @app.route('/venue/<int:id>/')
 def venue_info(id, name=None):
-    return repr(controller.get_venue_by_id(id))
+    return repr(model.get_venue_by_id(id))
 
 @app.route('/venue/add/', methods=['GET', 'POST'])
 def venue_add():
     name = request.form.get('name')
     address = request.form.get('address')
     if name and address:
-        controller.add_venue(name, address)
+        model.add_venue(name, address)
         return redirect(url_for('venue'))
     else:
         template = templates.get_template("venue_add.html")
@@ -72,7 +72,7 @@ def venue_add():
 def venue_delete():
     id = request.form.get('id')
     if id:
-        controller.delete_venue(id)
+        model.delete_venue(id)
         return redirect(url_for('venue'))
     else:
         template = templates.get_template("venue_delete.html")
@@ -82,31 +82,31 @@ def venue_delete():
 @app.route('/gig/')
 def gig():
     template = templates.get_template("gig.html")
-    return template.render(gigs=controller.get_gigs())
+    return template.render(gigs=model.get_gigs())
 
 @app.route('/gig/<int:id>')
 def gig_info(id, name=None):
-    return repr(controller.get_gig_by_id(id))
+    return repr(model.get_gig_by_id(id))
 
 @app.route('/gig/add/', methods=['GET', 'POST'])
 def gig_add():
     if 'time_start' in request.form and 'venue' in request.form and 'artists' in request.form:
         time_start = datetime.datetime.strptime(request.form.get('time_start'), '%Y-%m-%d %H:%M')
-        venue = controller.get_venue_by_name(request.form.get('venue'))
+        venue = model.get_venue_by_name(request.form.get('venue'))
         if not venue:
-            controller.add_venue(request.form.get('venue'), 'Unknown')
-        venue = controller.get_venue_by_name(request.form.get('venue'))
+            model.add_venue(request.form.get('venue'), 'Unknown')
+        venue = model.get_venue_by_name(request.form.get('venue'))
         artist_ids = []
         for artist in request.form.getlist('artists'):
             if not artist: continue
-            a = controller.get_artist_by_name(artist)
+            a = model.get_artist_by_name(artist)
             if a:
                 artist_ids.append(a.id)
             else:
-                controller.add_artist(artist)
-                a = controller.get_artist_by_name(artist)
+                model.add_artist(artist)
+                a = model.get_artist_by_name(artist)
                 artist_ids.append(a.id)
-        controller.add_gig(time_start, venue.id, artist_ids)
+        model.add_gig(time_start, venue.id, artist_ids)
         return redirect(url_for('gig'))
     else:
         template = templates.get_template("gig_add.html")
@@ -116,7 +116,7 @@ def gig_add():
 def gig_delete():
     id = request.form.get('id')
     if id:
-        controller.delete_gig(id)
+        model.delete_gig(id)
         return redirect(url_for('gig'))
     else:
         template = templates.get_template("gig_delete.html")
--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 BROKER_URL = 'sqla+sqlite:///celerydb-tasks.sqlite'
 BACKEND_URL = 'sqla+sqlite:///celerydb-results.sqlite'
 
-import controller, lastfm_api
+import model, lastfm_api
 
 from celery import Celery
 from celery.utils.log import get_task_logger
@@ -15,9 +15,9 @@ celery.conf.CELERY_RESULT_DBURI = "sqlite:///celerydb-results.sqlite"
 # eg tasks.update_artist_data.delay('twerps')
 @celery.task
 def update_artist_data(name):
-    original_record = controller.get_artist_by_name(name)
+    original_record = model.get_artist_by_name(name)
     if original_record:
         info = lastfm_api.get_artist_info(name)
-        controller.update_artist_by_id(original_record.id, name=info.name, bio=info.bio.content)
+        model.update_artist_by_id(original_record.id, name=info.name, bio=info.bio.content)
     else:
         return "Couldn't update artist data for %s as no record matches that name!" % name
