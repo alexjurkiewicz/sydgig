3fc8068fa0bd906ec8f01cf483f50bdccb578626
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-09-30 16:36:17 +1000

    test.py should support adding events to an arbitrary number of days into the future

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import random, string, datetime
 N_RANDOM_ARTISTS = 20
 N_RANDOM_VENUES = 5
 N_RANDOM_GIGS = 32
+N_FUTURE_GIG_DAYS = 30
 
 def random_artist():
     return "The %ss" % (''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(random.randint(4, 7))).title())
@@ -17,8 +18,8 @@ for i in range(N_RANDOM_ARTISTS):
 print "Adding %s random venues..." % N_RANDOM_VENUES
 for i in range(N_RANDOM_VENUES):
     model.add_venue(random_venue())
-print "Adding %s random gigs in the next 14 days..." % N_RANDOM_GIGS
+print "Adding %s random gigs in the next %s days..." % (N_RANDOM_GIGS, N_FUTURE_GIG_DAYS)
 for i in range(N_RANDOM_GIGS):
     artists = random.sample(model.get_artists(), random.randint(2, 5))
     venue = random.choice(model.get_venues())
-    model.add_gig(time_start = datetime.datetime.now() + datetime.timedelta(days=random.randint(0,14)), venue_id = venue.id, artist_ids = [artist.id for artist in artists], name="%s album launch" % (artists[0].name))
+    model.add_gig(time_start = datetime.datetime.now() + datetime.timedelta(days=random.randint(0,N_FUTURE_GIG_DAYS)), venue_id = venue.id, artist_ids = [artist.id for artist in artists], name="%s album launch" % (artists[0].name))
