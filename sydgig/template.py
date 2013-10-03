11dd2ed11c2f3b8270b50cde92e0802abb3eabfb
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-10-03 13:13:15 +1000

    Cosmetics in template.py and convert istoday from global variable to filter

--- sydgig/template.py
+++ sydgig/template.py
@@ -8,19 +8,28 @@ def guess_autoescape(template_name):
     ext = template_name.rsplit('.', 1)[1]
     return ext in ('html', 'htm', 'xml')
 
+templates = Environment(loader=FileSystemLoader('sydgig/template/'),
+                        autoescape=guess_autoescape,
+                        extensions=['jinja2.ext.autoescape'])
+
+# Filters
 def quote_plus(value):
     return urllib.quote_plus(value)
+templates.filters['quote_plus'] = quote_plus
 
 def naturaltime(value):
     return humanize.naturaltime(value)
+templates.filters['naturaltime'] = naturaltime
 
 def simpledate(value):
     '''datetime -> Sat 3'''
     return value.strftime('%a ') + str(int(value.strftime('%d'))) # gets rid of leading 0
+templates.filters['simpledate'] = simpledate
 
 def longdate(value):
     '''datetime -> Friday 6 September, 2013'''
     return value.strftime('%A %d %B, %Y')
+templates.filters['longdate'] = longdate
 
 def truncate_list(l, maxitems=3, end='...'):
     '''Return a list of max length maxitems, with the last item as end if there were more in the first place'''
@@ -28,17 +37,15 @@ def truncate_list(l, maxitems=3, end='...'):
         return l[:maxitems] + [end]
     else:
         return l
-
-templates = Environment(loader=FileSystemLoader('sydgig/template/'),
-                        autoescape=guess_autoescape,
-                        extensions=['jinja2.ext.autoescape'])
-
-templates.filters['quote_plus'] = quote_plus
-templates.filters['naturaltime'] = naturaltime
-templates.filters['simpledate'] = simpledate
-templates.filters['longdate'] = longdate
 templates.filters['truncate_list'] = truncate_list
 
+def istoday(d):
+    '''return true if datetime d is the same day as today'''
+    n = datetime.datetime.now()
+    return d.year == n.year and d.month == n.month and d.day == n.day
+templates.filters['istoday'] = istoday
+
+# Global variables
 def date_year():
     return int(time.strftime("%Y"))
 def date_month():
@@ -47,11 +54,6 @@ def date_day():
     return int(time.strftime("%d")) # int() also strips the leading zero here
 def date_today():
     n = datetime.datetime.now()
-    #return datetime.datetime(n.year, n.month, n.day, 0, 0)
     return datetime.date(n.year, n.month, n.day)
-def istoday(d):
-    '''return true if datetime d is the same day as today'''
-    n = datetime.datetime.now()
-    return d.year == n.year and d.month == n.month and d.day == n.day
-templates.globals.update(date_year=date_year, date_month=date_month, date_day=date_day, date_today=date_today, istoday=istoday)
+templates.globals.update(date_year=date_year, date_month=date_month, date_day=date_day, date_today=date_today)
 
